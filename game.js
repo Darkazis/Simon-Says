296025366335bc29c68492a0820bb1fbe2fb1521
Darkazis <61381823+Darkazis@users.noreply.github.com>
2021-08-14 17:43:02 +1000

    Learnt how to make the Simon Game Challenge

---
+++ game.js
@@ -0,0 +1,86 @@
+
+
+const buttonColours = ["red", "blue", "green", "yellow"]
+
+let gamePattern = [];
+
+let userClickedPattern = [];
+
+let level = 0;
+
+let started = false;
+
+$(document).keypress(function () {
+  if(!started) {
+    started = true;
+    nextSequence();
+    $("h1").text("level " + level);
+  }
+});
+
+$(".btn").click(function (event) {
+  let userChosenColour = event.target.id;
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
+})
+
+function nextSequence() {
+
+  userClickedPattern = [];
+
+  let randomNumber = Math.floor(( Math.random() * 4 ));
+  let randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+
+  level++
+  $("h1").text("level " + level);
+
+}
+
+function playSound(name) {
+  let audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour) {
+  $("." + currentColour).addClass("pressed");
+  setTimeout(function () {
+    $("." + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer (currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  }
+  else {
+    playSound("wrong")
+    $('body').addClass("game-over");
+    setTimeout(function () {
+      $('body').removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+    console.log("wrong");
+  }
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
